d38a182b08e91210bbb38476fe52840a9e5db5d1
Danchao666 <85376458+Danchao666@users.noreply.github.com>
2021-06-06 13:45:01 +0700

    Update st.sh

--- flaskapp/st.sh
+++ flaskapp/st.sh
@@ -1,9 +1,9 @@
 gunicorn --bind 127.0.0.1:5000 wsgi:app & APP_PID=$!
-sleep 25
+sleep 25[s]
 echo start client
 python3 client.py
 APP_CODE=$?
-sleep 5
+sleep 5[s]
 echo $APP_PID 
 kill -TERM $APP_PID
 echo app code $APP_CODE
